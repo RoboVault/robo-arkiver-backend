544cb8671e0bc334851108d96f1005b1bea12e03
hazelnutcloud <ha5anmukhli50@gmail.com>
2023-03-24 22:57:36 +0800

    bump version

--- manager/deps.ts
+++ manager/deps.ts
@@ -1,6 +1,6 @@
-export * as arkiver from "../../RoboArkiver/mod.ts";
-export * as arkiverTypes from "../../RoboArkiver/src/arkiver/types.ts";
-export * as arkiverUtils from "../../RoboArkiver/src/utils.ts";
+export * as arkiver from "https://deno.land/x/robo_arkiver@v0.1.8/mod.ts";
+export * as arkiverTypes from "https://deno.land/x/robo_arkiver@v0.1.8/src/arkiver/types.ts";
+export * as arkiverUtils from "https://deno.land/x/robo_arkiver@v0.1.8/src/utils.ts";
 export * as supabase from "https://esm.sh/@supabase/supabase-js@2.5.0";
 export * as mongoose from "npm:mongoose";
 export * as graphQLCompose from "npm:graphql-compose";
