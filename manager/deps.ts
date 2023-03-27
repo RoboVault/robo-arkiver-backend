7a6e2fe4117653726ede8e3dcbf151a1d96f781d
hazelnutcloud <ha5anmukhli50@gmail.com>
2023-03-27 12:19:51 +0800

    bump dependancy

--- manager/deps.ts
+++ manager/deps.ts
@@ -1,6 +1,6 @@
-export * as arkiver from "https://deno.land/x/robo_arkiver@v0.1.8/mod.ts";
-export * as arkiverTypes from "https://deno.land/x/robo_arkiver@v0.1.8/src/arkiver/types.ts";
-export * as arkiverUtils from "https://deno.land/x/robo_arkiver@v0.1.8/src/utils.ts";
+export * as arkiver from "https://deno.land/x/robo_arkiver@v0.1.9/mod.ts";
+export * as arkiverTypes from "https://deno.land/x/robo_arkiver@v0.1.9/src/arkiver/types.ts";
+export * as arkiverUtils from "https://deno.land/x/robo_arkiver@v0.1.9/src/utils.ts";
 export * as supabase from "https://esm.sh/@supabase/supabase-js@2.5.0";
 export * as mongoose from "npm:mongoose";
 export * as graphQLCompose from "npm:graphql-compose";
