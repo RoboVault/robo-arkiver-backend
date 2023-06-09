fa18324fec310a84d11ecd5a251d1aa31e009a60
SmoothBot <smoothbot123@gmail.com>
2023-06-09 09:04:53 +0800

    fix: Arkives not starting

--- manager/packages/manager/manager.ts
+++ manager/packages/manager/manager.ts
@@ -60,7 +60,7 @@ export class ArkiveManager {
 
 			this.faultyArkives = await FaultyArkives.create(this.retryFaultyArkive.bind(this))
 			await Promise.all([
-				deployments.map(arkive => this.faultyArkives?.updateDeploymentStatus(arkive, 'error'))
+				deployments.map(arkive => this.addNewDeployment(arkive))
 			])
 
 		} catch (e) {
