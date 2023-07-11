f18166a9178c8b3509ca2faa36db5b979546c577
hazelnutcloud <hsnmkls@proton.me>
2023-07-12 00:18:38 +0800

    fix logs edge function

--- supabase/functions/logs/index.ts
+++ supabase/functions/logs/index.ts
@@ -1,6 +1,8 @@
-import { cors, Hono, InfluxDB, serve } from '../_shared/deps.ts'
+import { cors, Hono, InfluxDB, serve, validator } from '../_shared/deps.ts'
 
 const influxDBUrl = 'http://ec2-3-238-164-39.compute-1.amazonaws.com:8086/'
+const influxDBStagingUrl =
+	'http://ec2-54-174-96-237.compute-1.amazonaws.com:8086/'
 const influxDBOrg = 'robolabs'
 
 const getLimitOffset = (page: number) => {
@@ -26,52 +28,72 @@ const app = new Hono()
 			],
 		}),
 	)
-	.get('/logs/:arkiveId/:version', async (c) => {
-		const influxDBToken = Deno.env.get('INFLUXDB_TOKEN')
-		if (!influxDBToken) {
-			c.status(500)
-			return c.text(`INFLUXDB_TOKEN not set`)
-		}
+	.basePath('/logs')
+	.get(
+		'/:arkiveId/:version',
+		validator('query', (value) => {
+			if (!value['env']) {
+				value['env'] = 'staging'
+			}
+			// deno-lint-ignore ban-types
+			return value as { env: 'prod' | 'staging' | string & {} }
+		}),
+		async (c) => {
+			const { env } = c.req.valid('query')
+			let influxDBToken
+			if (env === 'staging') {
+				influxDBToken = Deno.env.get('INFLUXDB_STAGING_TOKEN')
+			} else if (env === 'prod') {
+				influxDBToken = Deno.env.get('INFLUXDB_TOKEN')
+			} else {
+				return c.text('Invalid env', 400)
+			}
 
-		const { arkiveId, version } = c.req.param()
-		const { start, stop, source, level, page } = c.req.query()
+			if (!influxDBToken) {
+				c.status(500)
+				return c.text(`INFLUXDB_TOKEN not set`)
+			}
 
-		const splitVersion = version.split('.')
-		if (splitVersion.length !== 2) {
-			c.status(400)
-			return c.text(`version must be in the format of major.minor`)
-		}
+			const { arkiveId, version } = c.req.param()
+			const { start, stop, source, level, page } = c.req.query()
 
-		const { limit, offset } = getLimitOffset(parseInt(page ?? 0))
+			const splitVersion = version.split('.')
+			if (splitVersion.length !== 2) {
+				c.status(400)
+				return c.text(`version must be in the format of major.minor`)
+			}
 
-		const queryApi = new InfluxDB({
-			url: influxDBUrl,
-			token: influxDBToken,
-		}).getQueryApi(influxDBOrg)
+			const { limit, offset } = getLimitOffset(parseInt(page ?? 0))
 
-		const query = `
+			const queryApi = new InfluxDB({
+				url: env == 'prod' ? influxDBUrl : influxDBStagingUrl,
+				token: influxDBToken,
+			}).getQueryApi(influxDBOrg)
+
+			const query = `
 			from(bucket: "arkiver_logs")
 				|> range(start: ${start ?? 0}, stop: ${
-			stop ?? new Date().toISOString()
-		})
+				stop ?? new Date().toISOString()
+			})
 				|> filter(fn: (r) => r["_measurement"] == "arkive_log")
 				|> filter(fn: (r) => r["_field"] == "message")
 				|> filter(fn: (r) => r["id"] == "${arkiveId}")
 				|> filter(fn: (r) => r["majorVersion"] == "${splitVersion[0]}")
 				|> filter(fn: (r) => r["minorVersion"] == "${splitVersion[1]}")
-				|> filter(fn: (r) => r["source"] == "${source ?? 'arkiver'}")
+				|> filter(fn: (r) => r["source"] == "${source ?? 'arkive'}")
 				|> filter(fn: (r) => r["level_name"] == "${level ?? 'INFO'}")
 		    |> sort(columns: ["_time"], desc: true)
 				|> limit(n: ${limit}, offset: ${offset})
 		`
 
-		console.log(query)
+			console.log(query)
 
-		const result = await queryApi.collectRows(query)
+			const result = await queryApi.collectRows(query)
 
-		c.status(200)
-		return c.json(result)
-	})
+			c.status(200)
+			return c.json(result)
+		},
+	)
 	.options('*', (c) => {
 		c.status(200)
 		return c.text('ok')
