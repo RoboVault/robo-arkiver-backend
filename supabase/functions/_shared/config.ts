aed8aeb6b153e0a12442e5fc19c1a41864037ba4
SmoothBot <smoothbot123@gmail.com>
2023-08-16 10:58:24 +1000

    fixing test

--- supabase/functions/_shared/config.ts
+++ supabase/functions/_shared/config.ts
@@ -1,5 +1,5 @@
 import { load } from "https://deno.land/std@0.173.0/dotenv/mod.ts";
 import { getEnv } from "./utils.ts";
 if (getEnv('ENVIRONMENT') === 'dev') {
-  await load()
+  await load({ export: true })
 }
