d3396e2081c4fb63f2a3e6c79a5fecd45160145e
pepper24Bot <jen@robolabs.biz>
2023-09-07 02:15:34 +0800

    Add Negative path testing

--- supabase/test/logs.test.ts
+++ supabase/test/logs.test.ts
@@ -5,10 +5,10 @@ import {
 import 'https://deno.land/std@0.173.0/dotenv/load.ts'
 import { getEnv } from '../functions/_shared/utils.ts'
 
-const LOGS_URL = 'http://localhost:8000/logs'
+// const LOGS_URL = 'http://localhost:8000/logs'
 
 // TODO: Remove this - do not use this
-// const LOGS_URL = 'https://egutsbjqffrqmhqbbfia.functions.supabase.co/logs-test'
+const LOGS_URL = 'https://egutsbjqffrqmhqbbfia.functions.supabase.co/logs-test'
 
 const ANON_KEY = getEnv('SUPABASE_ANON_KEY')
 const headers = (token: string = ANON_KEY) => {
@@ -18,33 +18,30 @@ const headers = (token: string = ANON_KEY) => {
   }
 }
 
-/** TODO Tests:
- * - Successfully reads logs
- * - all query params are optional
- * - Successful start and stop 
- * - Successful source
- * - Successful level
- * - Successful page
- * - Fails to read logs with invalid arkiveId
- * - Fails to read logs with invalid version
- * - Fails to read logs with invalid start
- * - Fails to read logs with invalid stop
- * - Fails to read logs with invalid source
- * - Fails to read logs with invalid level
- * - Fails to read logs with invalid page
- * - Fails to read logs with invalid env
- * - Fails to read logs with invalid token
+const baseSetup = (urlParam?: string) => {
+  const defaultUrl = `${LOGS_URL}/82/1.1`
+  const url = new URL(`${urlParam ? urlParam : defaultUrl}`)
+  const start = new Date(Date.now() - 1000 * 60 * 60 * 24 * 7)
+  const stop = new Date()
+
+  url.searchParams.append('start', start.toISOString())
+  url.searchParams.append('stop', stop.toISOString())
+
+  return url
+}
+
+/**
+ * Happy Path Tests:
+ * 1. GET Logs without any parameters
+ * 2. GET Logs with start and stop date parameters
+ * 3. GET Logs with valid severity parameters
+ * 4. GET Logs with valid source parameters
  */
 
 Deno.test({
   name: 'GET logs with no parameters',
   fn: async () => {
     const url = new URL(`${LOGS_URL}/82/1.1`)
-    const start = new Date(Date.now() - 1000 * 60 * 60 * 24 * 7)
-    const stop = new Date()
-
-    url.searchParams.append('start', start.toISOString())
-    url.searchParams.append('stop', stop.toISOString())
 
     const response = await fetch(url, {
       method: 'GET',
@@ -59,15 +56,9 @@ Deno.test({
 })
 
 Deno.test({
-  name: 'GET logs with empty severity parameters',
+  name: 'GET logs with start and stop date parameters',
   fn: async () => {
-    const url = new URL(`${LOGS_URL}/82/1.1`)
-    const start = new Date(Date.now() - 1000 * 60 * 60 * 24 * 7)
-    const stop = new Date()
-
-    url.searchParams.append('start', start.toISOString())
-    url.searchParams.append('stop', stop.toISOString())
-    url.searchParams.append('level', '[]')
+    const url = baseSetup()
 
     const response = await fetch(url, {
       method: 'GET',
@@ -77,9 +68,7 @@ Deno.test({
     assertEquals(response.status, 200)
 
     const res = await response.json()
-
-    // It should return empty since there is no severity provided
-    assertEquals(res.length, 0)
+    assertNotEquals(res.length, 0)
   },
 
   sanitizeResources: false,
@@ -89,12 +78,7 @@ Deno.test({
 Deno.test({
   name: 'GET logs with severity parameters',
   fn: async () => {
-    const url = new URL(`${LOGS_URL}/82/1.1`)
-    const start = new Date(Date.now() - 1000 * 60 * 60 * 24 * 7)
-    const stop = new Date()
-
-    url.searchParams.append('start', start.toISOString())
-    url.searchParams.append('stop', stop.toISOString())
+    const url = baseSetup()
     url.searchParams.append('level', '["INFO"]')
 
     const response = await fetch(url, {
@@ -132,42 +116,9 @@ Deno.test({
 })
 
 Deno.test({
-  name: 'GET logs with empty source parameters',
-  fn: async () => {
-    const url = new URL(`${LOGS_URL}/82/1.1`)
-    const start = new Date(Date.now() - 1000 * 60 * 60 * 24 * 7)
-    const stop = new Date()
-
-    url.searchParams.append('start', start.toISOString())
-    url.searchParams.append('stop', stop.toISOString())
-    url.searchParams.append('source', '[]')
-
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: headers(),
-    })
-
-    assertEquals(response.status, 200)
-
-    const res = await response.json()
-
-    // It should return empty since there is no source provided
-    assertEquals(res.length, 0)
-  },
-
-  sanitizeResources: false,
-  sanitizeOps: false,
-})
-
-Deno.test({
   name: 'GET logs with source parameters',
   fn: async () => {
-    const url = new URL(`${LOGS_URL}/82/1.1`)
-    const start = new Date(Date.now() - 1000 * 60 * 60 * 24 * 7)
-    const stop = new Date()
-
-    url.searchParams.append('start', start.toISOString())
-    url.searchParams.append('stop', stop.toISOString())
+    const url = baseSetup()
     url.searchParams.append('source', '["arkive"]')
 
     const response = await fetch(url, {
@@ -203,3 +154,216 @@ Deno.test({
   sanitizeResources: false,
   sanitizeOps: false,
 })
+
+/**
+ * Negative Test
+ * 
+ * - GET Logs of an invalid arkiveId
+ * - GET Logs of an invalid arkive version
+ * - GET Logs with inalid start and stop date
+ * - GET Logs with invalid source options
+ * - GET Logs with invalid level_name options
+ * - GET Logs with empty severity
+ * - GET Logs with empty source
+ * - GET Logs with invalid page
+ * - GET Logs with invalid token
+ * 
+ * TODO: 
+ * - Fails to read logs with invalid env
+ */
+
+Deno.test({
+  name: 'GET logs of an invalid arkiveId',
+  fn: async () => {
+    const url = baseSetup(`${LOGS_URL}/invalid-id/1.1`)
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: headers(),
+    })
+
+    // TODO: Probably better to return 404 error for specific arkive check
+    assertEquals(response.status, 200)
+
+    const res = await response.json()
+
+    // It should return empty since the id is not valid
+    assertEquals(res.length, 0)
+  },
+
+  sanitizeResources: false,
+  sanitizeOps: false,
+})
+
+Deno.test({
+  name: 'GET logs of an invalid version',
+  fn: async () => {
+    const url = baseSetup(`${LOGS_URL}/82/11`)
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: headers(),
+    })
+
+    assertEquals(response.status, 400)
+
+    const errorMessage = await response.text()
+
+    assertEquals(errorMessage, 'version must be in the format of major.minor')
+  },
+
+  sanitizeResources: false,
+  sanitizeOps: false,
+})
+
+Deno.test({
+  name: 'GET logs with invalid start and stop date',
+  fn: async () => {
+    const url = new URL(`${LOGS_URL}/82/1.1`)
+    const start = new Date(Date.now() - 1000 * 60 * 60 * 24 * 7)
+    const stop = new Date()
+
+    // invalid
+    url.searchParams.append('start', stop.toISOString())
+    url.searchParams.append('stop', start.toISOString())
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: headers(),
+    })
+
+    assertEquals(response.status, 500)
+  },
+
+  sanitizeResources: false,
+  sanitizeOps: false,
+})
+
+Deno.test({
+  name: 'GET logs with invalid source',
+  fn: async () => {
+    const url = baseSetup()
+    url.searchParams.append('source', '["invalid"]')
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: headers(),
+    })
+
+    assertEquals(response.status, 200)
+
+    const res = await response.json()
+
+    // It should return empty since there is no valid source provided
+    assertEquals(res.length, 0)
+  },
+
+  sanitizeResources: false,
+  sanitizeOps: false,
+})
+
+Deno.test({
+  name: 'GET logs with invalid severity',
+  fn: async () => {
+    const url = baseSetup()
+    url.searchParams.append('level', '["invalid"]')
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: headers(),
+    })
+
+    assertEquals(response.status, 200)
+
+    const res = await response.json()
+    assertEquals(res.length, 0)
+  },
+
+  sanitizeResources: false,
+  sanitizeOps: false,
+})
+
+Deno.test({
+  name: 'GET logs with empty severity parameters',
+  fn: async () => {
+    const url = baseSetup()
+    url.searchParams.append('level', '[]')
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: headers(),
+    })
+
+    assertEquals(response.status, 200)
+
+    const res = await response.json()
+
+    // It should return empty since there is no severity provided
+    assertEquals(res.length, 0)
+  },
+
+  sanitizeResources: false,
+  sanitizeOps: false,
+})
+
+Deno.test({
+  name: 'GET logs with empty source parameters',
+  fn: async () => {
+    const url = baseSetup()
+    url.searchParams.append('source', '[]')
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: headers(),
+    })
+
+    assertEquals(response.status, 200)
+
+    const res = await response.json()
+
+    // It should return empty since there is no source provided
+    assertEquals(res.length, 0)
+  },
+
+  sanitizeResources: false,
+  sanitizeOps: false,
+})
+
+Deno.test({
+  name: 'GET logs with invalid page',
+  fn: async () => {
+    const url = baseSetup()
+    url.searchParams.append('page', '-1')
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: headers(),
+    })
+
+    // Throw internal server error
+    assertEquals(response.status, 500)
+  },
+
+  sanitizeResources: false,
+  sanitizeOps: false,
+})
+
+Deno.test({
+  name: 'GET logs with invalid token',
+  fn: async () => {
+    const url = baseSetup()
+    url.searchParams.append('page', '-1')
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: headers('invalid'),
+    })
+
+    // Throw internal server error
+    assertEquals(response.status, 500)
+  },
+
+  sanitizeResources: false,
+  sanitizeOps: false,
+})
+
